da8623af357b49b89566ea59a789fbef3ef10e6f
AlvaroVFon <alvar_90_xti@hotmail.com>
2024-10-26 22:26:56 +0200

    Add basic scaffold

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-function main() {}
+import express from "express"
+process.loadEnvFile()
 
-main()
+const app = express()
+const PORT = process.env.PORT
+
+// Middlewares
+
+app.use(express.json())
+
+// Routes
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to Alvaro's App"
+  })
+})
+
+// Server
+
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`)
+})
